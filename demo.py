0303fabfa857f33a72f3aea772f1544af39f3a28
osatou0141 <99325404+osatou0141@users.noreply.github.com>
2022-03-29 20:32:17 +0900

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,9 @@
+import streamrit as st
+omedy.")genre = st.radio(
+    "What's your favorite movie genre",
+    ('Comedy', 'Drama', 'Documentary'))
+
+if genre == 'Comedy':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn't select comedy.")
