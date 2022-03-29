e3c86f505b9c00639bb26c7eecf0f9a8826ab6b3
osatou0141 <gbtikin@gmail.com>
2022-03-29 21:19:06 +0900

    update

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
 import streamlit as st
-genre = st.radio(
-    "What's your favorite movie genre",
-    ('Comedy', 'Drama', 'Documentary'))
-if genre == 'Comedy':
-    st.write('You selected Comedy.')
-if genre == 'Drama':
-    st.write('You selected Drama.')
-if genre == 'Documentary':
-    st.write('You selected Documentary')
+st.title("hello world!")
+# genre = st.radio(
+#     "What's your favorite movie genre",
+#     ('Comedy', 'Drama', 'Documentary'))
+# if genre == 'Comedy':
+#     st.write('You selected Comedy.')
+# if genre == 'Drama':
+#     st.write('You selected Drama.')
+# if genre == 'Documentary':
+#     st.write('You selected Documentary')
