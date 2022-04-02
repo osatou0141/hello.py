ea4762e1940577643a1c6280eccb7bd25de1e384
osatou0141 <gbtikin@gmail.com>
2022-04-02 16:37:45 +0900

    t

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title('QRコード生成')
 url = st.text_input('URLの入力：')
 if st.button('生成'):
-    _img = qrcode.make(url)  # type: ignore
+    _img = qrcode.make(url)
     _img.save('qrcode.png')
     img = Image.open('qrcode.png')
     st.image(img)
