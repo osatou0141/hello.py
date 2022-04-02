122dc63c720007f67f9f53c928b0c0a6b3c89ab2
osatou0141 <gbtikin@gmail.com>
2022-04-02 16:45:19 +0900

    t

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import qrcode
 from PIL import Image
+import numpy as np
+
 st.title('QRコード生成')
+
 url = st.text_input('URLの入力：')
+
 if st.button('生成'):
     _img = qrcode.make(url)
     _img.save('qrcode.png')
