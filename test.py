136a36cb71cf7a1480bea6c47e5ea3f7b8f07ec4
osatou0141 <gbtikin@gmail.com>
2022-04-02 15:27:47 +0900

    update

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import qrcode
+import numpy as np
 st.title('QRコード生成')
 url = st.text_input('URLの入力：')
 if st.button('生成'):
     img = qrcode.make(url)
-    st.image(img)
+    st.image(np.array(img))
