9801525bf8d2852736dfcf492028893a0ff69ff5
osatou0141 <gbtikin@gmail.com>
2022-04-12 19:04:51 +0900

    t

--- segno.py
+++ segno.py
@@ -1,8 +1,4 @@
-import segno
-import streamlit as st
-from PIL import Image
-segno_QRFILE = 'qrcode.png'
-qrcode = segno.make('Rain')
+import segno as sg
+qrcode = sg.make('hello')
 qrcode.save('qrcode.png')
-img = Image.open(segno_QRFILE)
-st.image(img)
+qrcode.show()
